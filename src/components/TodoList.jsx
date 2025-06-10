27fc88cd42e4a8b13247e294c26f09bd1bede74b
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-10 13:26:01 +0900

    feat: Optimize TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,22 @@
+import { memo } from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = ({ todos, onToggle, onDelete, onEdit }) => {
-    if (todos.length === 0) {
-        return <div className="py-4 text-center text-gray-500">할 일이 없습니다. 새로운 할 일을 추가해보세요!</div>;
-    }
-
+const TodoListComponent = ({ todos, onToggle, onDelete, onEdit }) => {
     return (
-        <div className="mt-4">
-            {todos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo} onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} />
-            ))}
-        </div>
+        <>
+            {todos.length === 0 ? (
+                <div className="py-4 text-center text-gray-500">할 일이 없습니다. 새로운 할 일을 추가해보세요!</div>
+            ) : (
+                <div className="mt-4">
+                    {todos.map((todo) => (
+                        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} />
+                    ))}
+                </div>
+            )}
+        </>
     );
 };
+
+const TodoList = memo(TodoListComponent);
 
 export default TodoList;
